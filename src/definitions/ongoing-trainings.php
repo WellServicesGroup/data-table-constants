5baa770d6b40489af05ca584940f1846e7b463c0
Pascal Drewes <pascal@drewes-webdesign.nl>
2017-08-16 09:00:21 +0200

    Add ongoing-trainings table

---
+++ src/definitions/ongoing-trainings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'columns' =>
+        [
+            'ongoing_trainings_employee' => 'employee',
+            'ongoing_trainings_training' => 'training',
+            'ongoing_trainings_from' => 'from',
+            'ongoing_trainings_to' => 'to',
+            'ongoing_trainings_options' => 'options',
+        ],
+];

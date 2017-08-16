422ad2e3386b1ec2a728c35eecd9b76dafb04f28
Pascal Drewes <pascal@drewes-webdesign.nl>
2017-08-16 11:56:44 +0200

    Add employee certificates table

---
+++ src/definitions/employee-certificates.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'columns' =>
+        [
+            'certificate_code' => 'certificate_code',
+            'certificate_name' => 'certificate_name',
+            'certificate_training_from' => 'training_from',
+            'certificate_training_to' => 'training_to',
+            'certificate_next_inspection_date' => 'next_inspection_date',
+            'certificate_due_in_days' => 'due_in_days',
+            'certificate_file' => 'certificate_file',
+        ],
+];
